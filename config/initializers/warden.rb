55c4282fede50706a3e1aa13669233060e06a392
Tuan Nguyen <amtuannguyen@gmail.com>
2022-09-20 14:37:36 -0400

    fix DEPRECATION WARNING: Initialization autoloaded the constants

--- config/initializers/warden.rb
+++ config/initializers/warden.rb
@@ -1,2 +1,4 @@
-Warden::Strategies.add(:ppy_auth, Warden::PpyAuthStrategy)
-Warden::Strategies.add(:barcode_auth, Warden::BarcodeAuthStrategy)
+Rails.application.reloader.to_prepare do
+  Warden::Strategies.add(:ppy_auth, Warden::PpyAuthStrategy)
+  Warden::Strategies.add(:barcode_auth, Warden::BarcodeAuthStrategy)
+end
--- config/initializers/z_transaction_logger.rb
+++ config/initializers/z_transaction_logger.rb
@@ -1,3 +1,5 @@
-logfile = File.open("#{Rails.root}/log/transactions.log", 'a')  # create log file
-logfile.sync = true  # automatically flushes data to file
-TLOG = TransactionLogger.new(logfile)  # constant accessible anywhere
+Rails.application.reloader.to_prepare do
+  logfile = File.open("#{Rails.root}/log/transactions.log", 'a')  # create log file
+  logfile.sync = true  # automatically flushes data to file
+  TLOG = TransactionLogger.new(logfile)  # constant accessible anywhere
+end
