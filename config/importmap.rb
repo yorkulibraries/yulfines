f9ee9a30ee5b2e9da91818486c0b2a3ae32ca02d
Tuan Nguyen <amtuannguyen@gmail.com>
2024-11-18 19:44:56 -0500

    add jquery datepicker and update report page

--- config/importmap.rb
+++ config/importmap.rb
@@ -3,3 +3,5 @@
 pin "application", preload: true
 pin "popper", to: 'popper.js', preload: true
 pin "bootstrap", to: 'bootstrap.min.js', preload: true
+pin "jquery", to: "https://cdn.jsdelivr.net/npm/jquery@3.6.0/dist/jquery.js"
+pin "datepicker", to: "https://cdn.jsdelivr.net/npm/bootstrap-datepicker@1.9.0/dist/js/bootstrap-datepicker.min.js"
