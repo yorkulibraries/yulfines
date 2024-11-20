d4d9aec2d2455e0847ea28a05182b1360a7cfc3b
Tuan Nguyen <amtuannguyen@gmail.com>
2024-11-20 07:07:21 -0500

    handle error 500 in ajax request

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -8,6 +8,11 @@ $(document).ready(function () {
    $('.datepicker').datepicker();
 
    if ($('#overview_fees_container').length > 0) {
-      $.getScript('/alma/reload_fees.js');
+      $.getScript('/alma/reload_fees.js').fail(function( jqxhr, settings, exception ) {
+         var error = '<h2 class="text-muted">' + exception + '</h2>';
+         var reload = '<p class="text-muted"><a href="#" onclick="window.location.reload(); return false;">Reload</a></p>';
+         var html = error + reload;
+         $('#overview_fees_container').empty().append(html);
+      });
    }
 });
