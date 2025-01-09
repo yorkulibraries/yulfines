f42df41f47fa554ccd337513168b8346eaab8489
Tuan Nguyen <amtuannguyen@gmail.com>
2025-01-09 07:35:02 -0500

    enable UJS

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,8 +1,10 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-
 import "bootstrap";
 import "jquery";
 import "datepicker";
+
+import Rails from '@rails/ujs';
+Rails.start();
 
 $(document).ready(function () {
   $('.datepicker').datepicker();
--- config/importmap.rb
+++ config/importmap.rb
@@ -5,3 +5,4 @@ pin "popper", to: 'popper.js', preload: true
 pin "bootstrap", to: 'bootstrap.min.js', preload: true
 pin "jquery", to: "https://cdn.jsdelivr.net/npm/jquery@3.6.0/dist/jquery.js"
 pin "datepicker", to: "https://cdn.jsdelivr.net/npm/bootstrap-datepicker@1.9.0/dist/js/bootstrap-datepicker.min.js"
+pin "@rails/ujs", to: "https://cdn.skypack.dev/@rails/ujs"
